bb31654af681e996dd5d2c2178874077d323a736
Noxus2007 <kevinandresreatiga@gmail.com>
2025-10-16 19:53:02 -0500

    Cambios

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-const tienda= [
+const tienda = [
     {
-     nombre: "hollow knight",
-     precio: 37.500,
+        nombre: "hollow knight",
+        precio: 37.500,
     },
     {
         nombre: "ULTRAKILL",
@@ -12,8 +12,8 @@ const tienda= [
         precio: 58.800
     },
     {
-nombre: "Project Zomboid",
-precio: 47.500,
+        nombre: "Project Zomboid",
+        precio: 47.500,
     },
     {
         nombre: "ARK: Survival Evolved",
@@ -21,22 +21,46 @@ precio: 47.500,
     }
 ]
 
-const tiendacontenedor = document.getElementById(shop);
+let carrito = []
+
+const tiendacontenedor = document.getElementById("shop");
 tienda.forEach((item) => {
     console.log("shop;", item);
-    
-const itemContenedor = document.createElement("div")
- tiendacontenedor.appendChild(itemContenedor)
-    
+
+    const itemContenedor = document.createElement("div")
+    tiendacontenedor.appendChild(itemContenedor)
     const itemNombre = document.createElement("span")
     itemContenedor.appendChild(itemNombre)
-   itemNombre.textContent = item.nombre
+    itemNombre.textContent = item.nombre
+    itemNombre.className = "Div"
 
 
-   const itemPrecio = document.createElement("span")
-   itemContenedor.appendChild(itemPrecio)
-   itemPrecio.textContent = item.precio + "$"
-    
-   const itemBoton = document.createElement("button")
+    const itemPrecio = document.createElement("span")
+    itemContenedor.appendChild(itemPrecio)
+    itemPrecio.textContent = item.precio + "$"
+
+    const itemBoton = document.createElement("button")
     itemContenedor.appendChild(itemBoton)
+    itemBoton.className = "boton"
+    itemBoton.textContent = "Agregar"
+
+
+    itemBoton.addEventListener("click", () => {
+        const existe = carrito.find((elemento) => {
+            return elemento.nombre == item.nombre
+        })
+
+        if (existe) {
+            existe.cantidad++
+        } else {
+
+            carrito.push({
+                nombre: item.nombre,
+                precio: item.precio,
+                cantidad: 1
+            })
+        }
+
+        console.log(carrito)
+    })
 })
